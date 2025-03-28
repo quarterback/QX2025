6c6abd780b3e0375e8044323517e65d742eab14e
quarterback <contact@ronbronson.com>
2025-03-27 23:15:56 -0700

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,18 +1,18 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
-	},
+  {
+    title: 'Home',
+    slug: 'https://consequencedesign.org',
+  },
+  {
+    title: 'Journal',
+    slug: '/journal',
+  },
+  {
+    title: 'About',
+    slug: '/company/about',
+  },
+  {
+    title: 'Ron Bronson',
+    slug: 'https://ronbronson.design',
+  },
 ];
